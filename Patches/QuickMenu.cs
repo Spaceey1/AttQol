21e204b6118815cc490b3b3433d6f72a6f18f8d9
spaceey1 <bruno@gadula.ca>
2025-02-28 05:33:32 +0100

    Removed unused code

--- Patches/QuickMenu.cs
+++ Patches/QuickMenu.cs
@@ -1,8 +1,6 @@
 using Alta.QuickAccessActions;
 using UnityEngine;
 using HarmonyLib;
-using Alta.Chunks;
-using Alta.Inventory;
 namespace Atthacc
 {
     public class ToggleMenu : QuickAccessMenuAction
@@ -16,17 +14,6 @@ namespace Atthacc
             Attqol.instance.isJumpActive = !Attqol.instance.isJumpActive;
         }
     }
-    public class SpawnMenu : QuickAccessMenuAction
-    {
-        public override bool IsValid => true;
-
-        public override bool IsActive => Attqol.instance.isJumpActive;
-        public override void Run(Controller controller)
-        {
-            Ingot ingot = CreateInstance<Ingot>();
-            SpawnHelper.SpawnPouchOrPickup(ingot.Item, 5, SpawnData.Default, controller.Entity.Chunk, controller.transform.position);
-        }
-    }
     [HarmonyPatch(typeof(QuickAccessMenuController))]
     public static class QuickAccessMenuControllerPatch{
         [HarmonyPrefix]
@@ -34,6 +21,5 @@ namespace Atthacc
         public static void AddCustomButton(ref QuickAccessMenuAction[] ___quickAccessMenus){
             ___quickAccessMenus = ___quickAccessMenus.AddToArray(ScriptableObject.CreateInstance<ToggleMenu>());
         }
-        
     }
 }
