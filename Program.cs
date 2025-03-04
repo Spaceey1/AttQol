68184a3d102d0f7f529139596bb947981b7e54b6
spaceey1 <bruno@gadula.ca>
2025-03-04 14:39:47 +0100

    Update version number

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Attqol
         public ConfigEntry<float> configTankTurnSensitivity;
         public const string pluginGuid = "space.att.attqol";
         public const string pluginName = "Attqol";
-        public const string pluginVersion = "0.0.2";
+        public const string pluginVersion = "0.0.3";
         Attqol()
         {
             instance = this;
